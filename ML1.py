72a801e873e342c0d73b65b56a3d3775073aab77
BhagyaChougule <52674980+BhagyaChougule@users.noreply.github.com>
2019-07-09 23:51:50 +0530

    Add files via upload

---
+++ ML1.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# # print("Hello Capstone Project Course!")
+
+# print("Hello Capstone Project Course!")
